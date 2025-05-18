90fad942c6a543fa1b59e9b34b10fad8b664c293
muhammadderic <muhammad.donny.e@gmail.com>
2025-05-18 10:14:17 +0700

    feat: create login method in auth store, add form section to login page

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -14,12 +14,18 @@ type SignupParams = {
   password: string;
 }
 
+type LoginParams = {
+  email: string;
+  password: string;
+}
+
 type AuthStore = {
   user: User | null;
   isAuthenticated: boolean;
   error: string | null;
   isLoading: boolean;
   signup: (params: SignupParams) => Promise<void>;
+  login: (params: LoginParams) => Promise<void>;
 }
 
 const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1/auth" : "/api/v1/auth";
@@ -43,6 +49,26 @@ export const useAuthStore = create<AuthStore>((set) => ({
       });
     } catch (error: any) {
       set({ error: error.response?.data?.message || "Error signing in", isLoading: false });
+      throw error;
+    }
+  },
+
+  login: async ({ email, password }: LoginParams) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const response = await axios.post(`${API_URL}/login`, { email, password });
+      set({
+        isAuthenticated: true,
+        user: response.data.user,
+        error: null,
+        isLoading: false,
+      });
+    } catch (error: any) {
+      set({
+        error: error.response?.data?.message || "Error logging in",
+        isLoading: false
+      });
       throw error;
     }
   },
