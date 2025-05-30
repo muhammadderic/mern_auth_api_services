a475d22b07bb7da545d67bb77f7090dcd57ba2a4
muhammadderic <muhammad.donny.e@gmail.com>
2025-05-30 10:44:03 +0700

    feat: update user model, create welcome email and template, create verify email handler and route

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
     verificationToken: String,
     verificationTokenExpiresAt: Date,
   },
