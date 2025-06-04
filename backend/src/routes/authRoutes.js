8eb003265a88f0e81c75ad7b75c307040d3c2b26
muhammadderic <muhammad.donny.e@gmail.com>
2025-06-04 10:07:03 +0700

    feat: create login and logout handlers and routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 import {
   signup,
+  login,
+  logout,
 } from "../controllers/authController.js";
 
 router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
